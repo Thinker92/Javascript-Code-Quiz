419319650b5f590c156e335e9a0c2c780f860957
Thinker92 <vilsonapo@gmail.com>
2022-01-23 15:38:18 -0500

    Added pseudo code to script.js file

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Event listner for start button
+
+// Start function: clears main div and displays question and 4 answers from array, starts timer countdown
+
+// 4 answers button click listener, then pause timer and show highlight correct/incorrect for 3-4 second delay
+// After answer, start timer and display next question
+
+// After final question, display quiz finished, score, add input for initials + Save Score button
+
+// Display High score list, clear scores button, retake quiz button
