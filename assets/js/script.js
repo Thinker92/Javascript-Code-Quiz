e719b6d0245a9d0fdb6dbeff2375f5554dd0b289
Thinker92 <vilsonapo@gmail.com>
2022-02-14 16:40:43 -0500

    Developing startQuiz function. Removing main screen and displaying question screen with answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,21 @@
 // Decalring DOM elements
+var body = document.querySelector("body");
 var viewHighScoreLink = document.querySelector("#viewScores");
 var clearHighScoreLink = document.querySelector("#clearScores");
 var startBtn = document.querySelector("#startBtn");
 var mainEl = document.querySelector("#main");
 var mainTitle = document.querySelector("#mainTitle");
 var mainText = document.querySelector("#mainText");
-var timerVal = document.querySelector("timerValue");
+var timerEL = document.querySelector("#timerValue");
+var timer = 90;
+
+// Declaring buttons
+
+viewHighScoreLink.addEventListener("click", viewScores);
+
+clearHighScoreLink.addEventListener("click", clearScores);
+
+startBtn.addEventListener("click", startQuiz);
 
 var dataArray = [
   {
@@ -59,12 +69,7 @@ var dataArray = [
   },
 ];
 
-// Start function: clears main div and displays question and 4 answers from array, starts timer countdown
-
 // 4 answers button click listener, then pause timer and show highlight correct/incorrect for 3-4 second delay
-// After answer, start timer and display next question
-
-// After final question, display quiz finished, score, add input for initials + Save Score button
 
 // Display High score list, clear scores button, start quiz button
 function viewScores() {
@@ -76,27 +81,57 @@ function clearScores() {
   event.preventDefault();
   alert("Clicked!");
 }
+
+// Start function: clears main div and displays question and 4 answers from array, starts timer countdown
 function startQuiz() {
   event.preventDefault();
-
-  for (var i = 0, y = 1; i < dataArray.length; i++) {
-    // console.log(dataArray[i]); // returns 3
-    console.log(dataArray[i].questionText);
-    console.log(dataArray[i].answers[y]);
-    console.log(dataArray[i].answers[y + 1]);
-    console.log(dataArray[i].answers[y + 2]);
-    console.log(dataArray[i].answers[y + 3]);
-    // for (var y = 1; y - 1 < dataArray[i].answers[y]; i++) {
-    //   console.log(dataArray[i].answers(y));
-    // }
-    // mainTitle.textContent = dataArray[0].answers[0];
-    // mainText.textContent = dataArray.question.answers[0];
+  mainEl.remove();
+  timerEL.textContent = timer;
+  var questionsEl = document.createElement("div");
+  questionsEl.setAttribute("id", "questions");
+  questionsEl.setAttribute(
+    "class",
+    "is-flex is-align-items-center is-flex-direction-column my-6 px-6"
+  );
+  function quesitonItem(i) {
+    return dataArray[i].questionText;
   }
+  function answerItem(i, j) {
+    return dataArray[i].answers[j];
+  }
+
+  var q1Title = document.createElement("h1");
+  q1Title.setAttribute("id", "questionTitle");
+  q1Title.textContent = quesitonItem(1);
+
+  var answerEl1 = document.createElement("button");
+  answerEl1.setAttribute("id", "answer1");
+  answerEl1.setAttribute(
+    "class",
+    "button is-medium is-responsive is-info is-light"
+  );
+  answerEl1.textContent = answerItem(1, 1);
+
+  questionsEl.appendChild(q1Title);
+  questionsEl.appendChild(answerEl1);
+  body.appendChild(questionsEl);
+  // answerEl1.textContent = dataArray[question[1]].answers[1];
+
+  // for (var i = 0; dataArray.length; i++) {
+  //   console.log(dataArray[i]);
+  // }
+  //   console.log(dataArray[i].answers[1]);
+  //   console.log(dataArray[i].answers[2]);
+  //   console.log(dataArray[i].answers[3]);
+  //   console.log(dataArray[i].answers[4]);
+  //   dataArray[i].answered = true;
+  //   // for (var y = 1; y - 1 < dataArray[i].answers[y]; i++) {
+  //   //   console.log(dataArray[i].answers(y));
+  //   // }
+  //   // mainTitle.textContent = dataArray[0].answers[0];
+  //   // mainText.textContent = dataArray.question.answers[0];
+  // }
+  // After answer, start timer and display next question
+
+  // Display quiz finished, score, add input for initials + Save Score button
 }
-
-// Event listners
-viewHighScoreLink.addEventListener("click", viewScores);
-
-clearHighScoreLink.addEventListener("click", clearScores);
-
-startBtn.addEventListener("click", startQuiz);
